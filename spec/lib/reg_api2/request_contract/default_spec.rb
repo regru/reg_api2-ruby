c05d369ccb8231187abb49353ab851db2b98ff20
Akzhan Abdulin <akzhan.abdulin@gmail.com>
2013-09-07 20:50:26 +0400

    More specs.

---
+++ spec/lib/reg_api2/request_contract/default_spec.rb
@@ -0,0 +1,20 @@
+# -*- encoding : utf-8 -*-
+describe RegApi2::RequestContract::Default do
+
+  let!(:contract) {
+    RegApi2::RequestContract::Default.new(
+      required: %w[ a b ],
+      optional: %w[ c d ]
+    )
+  }
+
+  describe :initialize do
+    it "should assign opts" do
+      contract.opts.should == {
+        required: %w[ a b ],
+        optional: %w[ c d ]
+      }
+    end
+  end
+
+end
