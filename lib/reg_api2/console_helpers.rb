f5f52eaba0099d2c6cfca2e59772f57e7ddcd7b7
Akzhan Abdulin <akzhan.abdulin@gmail.com>
2013-09-11 23:24:31 +0400

    Try to load username/password from ~/.regapi2 when exists.

--- lib/reg_api2/console_helpers.rb
+++ lib/reg_api2/console_helpers.rb
@@ -3,5 +3,22 @@ require 'reg_api2'
 
 include RegApi2
 
-RegApi2.username = 'test'
-RegApi2.password = 'test'
+# try to load defaults from ~/.regapi2
+def try_lo_load_defaults
+  filename = "#{ENV['HOME']}/.regapi2"
+  username = 'test'
+  password = 'test'
+
+  if File.readable?(filename)
+    IO.read(filename).split("\n").each do |line|
+      username = $1  if line.strip =~ /^\s*username\s*\=(.+)\s*$/;
+      password = $1  if line.strip =~ /^\s*password\s*\=(.+)\s*$/;
+    end
+  end
+
+  RegApi2.username = username
+  RegApi2.password = password
+  puts "Your default username: \"#{username}\", and You can change it with \"#{filename}\"."
+end
+
+try_lo_load_defaults
